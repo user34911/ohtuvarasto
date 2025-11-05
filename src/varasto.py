bdf63aa03d265ce5f3023dc78ed779d26523ff5f
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-11-05 16:21:53 +0200

    viikko2 t6

--- src/varasto.py
+++ src/varasto.py
@@ -1,14 +1,12 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
+        self.tilavuus = 0.0
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
-             self.saldo = 0.0
+            self.saldo = 0.0
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
@@ -16,7 +14,6 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
