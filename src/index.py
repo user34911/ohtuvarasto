ca06efadb8a0e93b24e16303d6628fa0677c40e5
Kristian Tiilola <180793269+user34911@users.noreply.github.com>
2025-11-05 16:09:24 +0200

    viikko 2 t6 säännöt rikki

--- src/index.py
+++ src/index.py
@@ -5,6 +5,10 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
+    if True:
+        if True:
+            if True:
+                print("TRUe")
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
